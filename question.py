50c2b6b72b0864ecb3fc3073379d521e1720c4cf
qLigature <wilson.reyes@obf.ateneo.edu>
2021-11-10 18:58:34 +0800

    Added interactive menu, refactored input

--- question.py
+++ question.py
@@ -1,16 +1,84 @@
 from random import choice as ch
 
 mode = {
-    "addition": "+",
-    "subtraction": "-",
-    "multiplication": "x",
-    "division": "/"
+    1: "+",
+    2: "-",
+    3: "x",
+    4: "/"
 }
+
+def hrz_line():
+    print("---------------------------------", end = "")
+    print("---------------------------------")
+
+def input_int():
+    while True:
+        try:
+            num = input(">> ")
+            num = int(num)
+            return num
+        except ValueError:
+            print("Error! Please enter a valid number.")
+
+class GameManager:
+    
+    configs = {
+        "num1 diff": (2, 10),
+        "num2 diff": (2, 10)
+    }
+
+    def display_menu(self):
+        print("\t\t\t M A T H S L A Y E R\n")
+        print("[1] Play")
+        print("[2] Help")
+        print("[3] Options")
+        print("[4] Exit")
+
+        return input_int()
+    
+    def display_mode(self):
+        print("Choose your game mode:")
+        print("[1] Addition")
+        print("[2] Subtraction")
+        print("[3] Multiplication")
+        print("[4] Division")
+
+        return input_int()
+    
+    def display_help(self):
+        pass
+
+    def display_options(self):
+        pass
+
+    def display_exit(self):
+        print("Thank you for playing. See you next time!")
+
+class GameLoop:
+
+    def __init__(self, q_num: int, curr_mode: str):
+        self.q_num = q_num
+        self.curr_mode = curr_mode
+        self.current_q = 0
+
+        num1 = range(1, 10) # difficulty
+        num2 = range(1, 10)
+        op = mode[self.curr_mode]
+        q_range = range(self.q_num)
+
+        self.q_list = [Question(ch(num1), ch(num2), op) for q in q_range]
+        
+    def ask_q(self):
+        current_q = self.q_list[Question.answered]
+
+        current_q.display()
+        answer = input_int()
+        current_q.check(answer)
 
 class Question:
 
-    answered = 0
-    answered_correct = 0
+    answered: int = 0
+    answered_correct: int = 0
 
     def __init__(self, num1: int, num2: int, operation: str):
         self.num1 = num1
@@ -43,49 +111,40 @@ class Question:
         return None
     
     def check(self, answer: int):
-        try:
-            answer = int(answer)
-        except ValueError:
-            print("Error! Please enter a valid answer.")
-            return False
-
         Question.answered += 1
         if answer != self.correct_answer:
             print("Wrong! The correct answer is {}.".format(
                 self.correct_answer))
-            return True
         else:
             print("Correct!")
             Question.answered_correct += 1
-            return True
 
-class GameLoop:
+menu = GameManager()
+hrz_line()
 
-    def __init__(self, q_num: int, curr_mode: str):
-        self.q_num = q_num
-        self.curr_mode = curr_mode
-        self.current_q = 0
-
-    def generate_q(self):
-        num1 = range(1, 10)
-        num2 = range(1, 10)
-        op = mode[self.curr_mode]
-        q_range = range(self.q_num)
-
-        self.q_list = [Question(ch(num1), ch(num2), op) for q in q_range]
+while True:
     
-    def ask_question(self):
-        question_done = False
-        self.q_list[Question.answered].display()
-        
-        while question_done == False:
-            answer = input(">> ")
-            question_done = self.q_list[Question.answered].check(answer)
-            
-        print("---------------------------------", end = "")
-        print("---------------------------------")
+    location = menu.display_menu()
 
-new_game = GameLoop(10, "division")
-new_game.generate_q()
-while new_game.q_num > Question.answered:
-    new_game.ask_question()
+    if location == 1:
+        hrz_line()
+        new_game = GameLoop(10, menu.display_mode())
+
+        while new_game.q_num > Question.answered:
+            hrz_line()
+            new_game.ask_q()
+    
+    elif location == 2:
+        menu.display_help()
+    
+    elif location == 3:
+        menu.display_options()
+        
+    elif location == 4:
+        menu.display_exit()
+        hrz_line()
+        exit()
+    
+    Question.answered = 0
+    Question.answered_correct = 0
+    hrz_line()
